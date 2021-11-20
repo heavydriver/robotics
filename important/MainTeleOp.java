a7e9fbb627f81abb77f71e3fea2b4e1897b7b825
heavydriver <78708990+heavydriver@users.noreply.github.com>
2021-11-20 00:01:35 -0600

    Add files via upload

---
+++ important/MainTeleOp.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "MainTeleOp")
+public class MainTeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("left_front");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("left_back");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("right_front");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("right_back");
+        CRServo carouselServo = hardwareMap.crservo.get("carousel_servo");
+        DcMotor arm = hardwareMap.dcMotor.get("arm_motor");
+        DcMotor wrist = hardwareMap.dcMotor.get("wrist_motor");
+        Servo intake = hardwareMap.servo.get("intake_servo");
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+            // carouselServo.setPower(gamepad1.right_stick_y);
+            if (gamepad1.b){
+                carouselServo.setPower(-1);
+            } else if (gamepad1.y){
+                carouselServo.setPower(0);
+            } else if (gamepad1.x){
+                carouselServo.setPower(1);
+            }
+
+            if (gamepad2.x){
+                carouselServo.setPower(1);
+            } else if (gamepad1.b) {
+                carouselServo.setPower(0.5);
+            }
+//            } else if (gamepad1.x){
+//                carouselServo.setPower(-1);
+//            }
+
+            arm.setPower(-gamepad2.left_stick_y);
+            wrist.setPower(-gamepad2.right_stick_y);
+
+
+
+//            if (gamepad1.left_trigger==1) {
+//                arm.setPower(1);
+//            } else if (gamepad1.left_trigger==1) {
+//                arm.setPower(-1);
+//            }
+        }
+    }
+}
